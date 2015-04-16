1ac209686fd8b451b15895b76881ade233b10bf5
juliaCottage <juliaCottage@users.noreply.github.com>
2015-04-16 14:08:23 -0600

    version 0.0.9

--- inc/new-event.php
+++ inc/new-event.php
@@ -1,7 +1,19 @@
+<?php
+// load the API Client library
+  include dirname(dirname(__FILE__)) . '/libraries/eventbrite/Eventbrite.php';
+
+  // Initialize the API client
+  //  Eventbrite API / Application key (REQUIRED)
+  //   http://www.eventbrite.com/api/key/
+  $api_key = 'E47AYMF35YRIXKWX2S';
+  //  Eventbrite user_key (OPTIONAL, only needed for reading/writing private user data)
+  //   http://www.eventbrite.com/userkeyapi
+  $user_key = '1423494773134672332572'; ?>
+
 <div class="wrap">
   <h2>Create Event</h2>
 
-  <form action="../process.php" method="post" name="myForm" id="create-event">
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" name="myForm" id="create-event">
 
     <table class="form-table">
       <tbody>
@@ -13,7 +25,7 @@
         </tr>
         <tr>
           <th scope="row"><label for="description">Description</label></th>
-          <td><textarea id="description" name="description" form="create-event"/></textarea>
+          <td><textarea id="description" name="description" form="create-event" rows="8" cols="100"/></textarea>
             <p class="description">Tell people what's special about this event.</p>
           </td>
         </tr>
@@ -57,27 +69,43 @@
     </form>
   </div>
 
-
   <?php
-  // load the API Client library
-  include dirname(dirname(__FILE__)) . '/libraries/eventbrite/Eventbrite.php';
+  // define variables and set to empty values
 
-  // Initialize the API client
-  //  Eventbrite API / Application key (REQUIRED)
-  //   http://www.eventbrite.com/api/key/
-  $api_key = 'E47AYMF35YRIXKWX2S';
-  //  Eventbrite user_key (OPTIONAL, only needed for reading/writing private user data)
-  //   http://www.eventbrite.com/userkeyapi
-  $user_key = '1423494773134672332572';
+  $name = $description = $start_date = $end_date = $zone = $privacy = $url = "";
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+     $name = test_input($_POST["name"]);
+     $description = test_input($_POST["description"]);
+     $start_date = test_input($_POST["start-date"]);
+     $end_date = test_input($_POST["end-date"]);
+     $zone = test_input($_POST["zone"]);
+     $privacy = test_input($_POST["privacy"]);
+     $url = test_input($_POST["url"]);
+  }
+
+  function test_input($data) {
+     $data = trim($data);
+     $data = stripslashes($data);
+     $data = htmlspecialchars($data);
+     return $data;
+  }
+
+
+
+
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   //see http://developer.eventbrite.com/doc/events/event_update/ for a
   // description of the available event_update parameters:
   $event_new_params = array(
-      'title' => 'my new event jvw',
-      'start_date' => '2015-11-01 22:30:00', // "YYYY-MM-DD HH:MM:SS"
-      'end_date' => '2015-11-02 23:30:00', // "YYYY-MM-DD HH:MM:SS"
-      'privacy' => 0,  // zero for private (not available in search), 1 for public (available in search)
-      'timezone' => 'America/Los_Angeles'
+      'title' => $name,
+      'description' => $description,
+      'start_date' => $start_date, // "YYYY-MM-DD HH:MM:SS"
+      'end_date' => $end_date, // "YYYY-MM-DD HH:MM:SS"
+      'privacy' => $privacy,  // zero for private (not available in search), 1 for public (available in search)
+      'timezone' => $zone
   );
 
   // initialize the API client
@@ -96,4 +124,5 @@
       echo "darn";
   }
   print var_dump($response);
+ }
   ?>
